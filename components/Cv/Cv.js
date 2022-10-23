0f0294f38ef8da7db0b84686369187a61a1cbedf
Kajsa Unge <hej@kajsaunge.se>
2022-10-23 07:53:23 +0200

    Add experience and education

--- components/Cv/Cv.js
+++ components/Cv/Cv.js
@@ -1,51 +1,37 @@
 import Section from "../Section/Section";
 import styles from "./Cv.module.css";
+import sectionStyles from "../Section/Section.module.css";
 import content from "../../pages/api/content.json";
 
-// .cv-content
-//     -var experienceList = []
-//     -for(var key in jsonExperience)
-//         -experienceList.push(jsonExperience[key])
-//     each experience in experienceList
-//         .cv-item
-//             p.cv-date= experience.date
-//             h3.cv-title= experience.title
-//             if !experience.companyLink
-//                 p= experience.company
-//             else
-//                 a.cv-link(href= experience.companyLink)= experience.company
-//             p.cv-description= experience.experienceDesc
-//             p.cv-learning= experience.experienceTakeaway
-// h2.section-title= json.generic.education
-// p.section-description= json.generic.educationText
-// .cv-content
-//     p.edu-category= json.generic.educationTypeProfessional
-//         -var educationList = []
-//         -for(var key in jsonEdu)
-//             -educationList.push(jsonEdu[key])
-//         each education in educationList
-//             .edu-item
-//                 p.cv-date= education.date
-//                 h5.edu-title= education.degree
-//                 if !education.schoolLink
-//                     p= education.school
-//                 else
-//                     a.link-yellow(href= education.schoolLink target="_blank")= education.school
-//     p.edu-category= json.generic.educationTypeOnline
-//         -var educationOnlineList = []
-//         -for(var key in jsonEduOnline)
-//             -educationOnlineList.push(jsonEduOnline[key])
-//         each educationOnline in educationOnlineList
-//             .edu-item
-//                 p.cv-date= educationOnline.date
-//                 h5.edu-title= educationOnline.degree
-//                 if !educationOnline.schoolLink
-//                     p= educationOnline.school
-//                 else
-//                     a.link-yellow(href= educationOnline.schoolLink target="_blank")= educationOnline.school
 const Cv = () => {
-  const { cv } = styles;
-  const { title, description, imgSrc, imgAlt } = content.cv;
+  const { sectionTitle, sectionDescription } = sectionStyles;
+  const {
+    cv,
+    cvItem,
+    date,
+    cvTitle,
+    link,
+    cvDescription,
+    learning,
+    eduCategory,
+    eduItem,
+    eduTitle,
+  } = styles;
+  const { title, description, imgSrc, imgAlt, experience, education } =
+    content.cv;
+  const experienceList = [];
+  for (const [key, value] of Object.entries(experience)) {
+    experienceList.push(experience[key]);
+  }
+  const eduList = [];
+  for (const [key, value] of Object.entries(education.schools)) {
+    eduList.push(education.schools[key]);
+  }
+  const eduOnlineList = [];
+  for (const [key, value] of Object.entries(education.educationOnline)) {
+    eduOnlineList.push(education.educationOnline[key]);
+  }
+
   return (
     <>
       <Section
@@ -54,7 +40,62 @@ const Cv = () => {
         imageAlt={imgAlt}
         imageSrc={imgSrc}
       >
-        <h3 className={cv}>List of work and edu </h3>
+        {experience &&
+          experienceList.map((item, i) => (
+            <>
+              <div key={i} className={cv}>
+                <div key={i} className={cvItem}>
+                  <p className={date}>{item.date}</p>
+                  <h3 className={cvTitle}>{item.title}</h3>
+                  {!experience.companyLink ? (
+                    <p>{item.company}</p>
+                  ) : (
+                    <a href={experience.companyLink} className={link}>
+                      {item.company}
+                    </a>
+                  )}
+                  <p className={cvDescription}>{item.experienceDesc}</p>
+                  <p className={learning}>{item.experienceTakeaway}</p>
+                </div>
+              </div>
+            </>
+          ))}
+        <h2 className={sectionTitle}>{education.title}</h2>
+        <p className={sectionDescription}>{education.description}</p>
+        <div className={cv}>
+          <p className={eduCategory}>{education.educationTypeProfessional}</p>
+          {eduList &&
+            eduList.map((item, i) => (
+              <div key={i} className={eduItem}>
+                <p className={date}>{item.date}</p>
+                <h5 className={eduTitle}>{item.title}</h5>
+                <p className={cvDescription}>{item.degree}</p>
+                {!item.schoolLink ? (
+                  <p>{item.school}</p>
+                ) : (
+                  <a href={item.schoolLink} className={link}>
+                    {item.school}
+                  </a>
+                )}
+              </div>
+            ))}
+          <p className={eduCategory}>{education.educationTypeOnline}</p>
+          {eduOnlineList &&
+            eduOnlineList.map((item, i) => (
+              <div key={i} className={eduItem}>
+                <p className={date}>{item.date}</p>
+                <h5 className={eduTitle}>{item.title}</h5>
+                <p className={cvDescription}>{item.degree}</p>
+                {!item.schoolLink ? (
+                  <p>{item.school}</p>
+                ) : (
+                  <a href={item.schoolLink} className={link}>
+                    {item.school}
+                  </a>
+                )}
+              </div>
+            ))}
+        </div>
       </Section>
     </>
   );
